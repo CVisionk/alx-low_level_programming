0ca573e10c7737dacbfc8c394bea3a4864fc68b2
Collen <damd819@gmail.com>
2024-01-23 01:11:02 +0200

    Structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef STRTYP
+#define STRTYP
+
+/**
+ * struct dog - a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: a header file for a dog struct
+ * Author: Jaba
+ * Date: Aug 29, 2022. @kp10B
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*STRTYP*/
+
