f4c6ed2abbf11050a142b96450246d90bd128211
Collen <damd819@gmail.com>
2024-01-24 21:08:26 +0200

    printName

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+    void print_name(char *name, void (*f)(char *)){
+        f(name);
+    }
