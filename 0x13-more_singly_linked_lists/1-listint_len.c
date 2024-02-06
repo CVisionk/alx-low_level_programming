7ce0186b1939cfc16128f4ef331ea1613f086c7b
Collen <damd819@gmail.com>
2024-02-06 20:49:32 +0200

    listLen

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,17 +3,17 @@
 /**
  * print_listint_len - returns the number of elements in a list.
  * @h: the start of the list
- * Return: number of nodes
+ * Return: number of elements
  */
 
 size_t listint_len(const listint_t *h)
 {
-	unsigned int nodes = 0;
+	unsigned int i = 0;
 
 	while (h != NULL)
 	{
-		nodes++;
+		i++;
 		h = h->next;
 	}
-	return (nodes);
+	return (i);
 }
