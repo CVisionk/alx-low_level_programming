60d21c46262ec2180ddb8d1f3020c7218a2ceb2b
Collen <damd819@gmail.com>
2024-02-14 16:50:51 +0200

    readfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 
 #include "main.h"
 /**
- * struct read_textfile -  reads a text file and prints it to the POSIX standard output
+ * struct read_textfile -  reads a text file and prints it
  * @filename: name of file
  * @letters:  number of letters it should read and print
  *
@@ -9,27 +9,29 @@
  *
  */
 
-
-ssize_t read_textfile(const char *filename, size_t letters){
-    char buffer[letters];
-    int fd;
-    ssize_t bytes_read = -1;
-    ssize_t bytes_written = -1;
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char buffer[letters];
+	int fd;
+	ssize_t bytes_read = -1;
+	ssize_t bytes_written = -1;
 
 	if (!filename)
 		return (0);
-    fd = open(filename, O_RDONLY);
-    if(fd == -1)
-    return (0);
-    bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1) {
-        close(fd);
-        return (0);
-    }
-    bytes_written = write(STDOUT_FILENO, buffer, letters);
-     if (bytes_written == -1) {
-        return (0);
-    }
-    close(fd);
-    return bytes_written;
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, letters);
+	if (bytes_written == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	return (bytes_written);
 }
