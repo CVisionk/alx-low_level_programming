3b830a27ecc3e22740cfccf0d3d5f7bcaa0c2c33
Collen <damd819@gmail.com>
2024-02-14 17:04:36 +0200

    readfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,40 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * struct read_textfile -  reads a text file and prints it
+ * @filename: name of file
+ * @letters:  number of letters it should read and print
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * return: written_bytes
+ *
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	char *buffer;
+    int fd;
+
+
+    buffer = malloc(sizeof(char) * (letters));
+    if (!buffer)
+		return (0);
+	ssize_t bytes_read, bytes_written;
 
 	if (!filename)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	}
+	bytes_written = write(STDOUT_FILENO, buffer, letters);
+	if (bytes_written == -1)
+	{
+		return (0);
+	}
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (bytes_written);
 }
