494974a158e8e88ad3b423ab8dc7868952d3ec5b
Collen <damd819@gmail.com>
2024-02-14 17:01:04 +0200

    readfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,36 @@
-
 #include "main.h"
-/**
- * struct read_textfile -  reads a text file and prints it
- * @filename: name of file
- * @letters:  number of letters it should read and print
- *
- * return: written_bytes
- *
- */
 
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-    int fd;
-
-
-    buffer = malloc(sizeof(char) * (letters));
-    if (!buffer)
-		return (0);
-	ssize_t bytes_read = -1;
-	ssize_t bytes_written = -1;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		close(fd);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, letters);
-	if (bytes_written == -1)
-	{
-		return (0);
-	}
-	close(fd);
-	return (bytes_written);
-}
 
-int main(){
-    read_textfile("letters.txt", 500);
-    return 0;
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
