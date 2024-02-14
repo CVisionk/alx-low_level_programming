c911d9c0c7bd18853017ffcb6206d82c3d6188bd
Collen <damd819@gmail.com>
2024-02-14 17:10:15 +0200

    readfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,18 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
     int fd;
+    ssize_t bytes_read, bytes_written;
 
+	if (!filename)
+		return (0);
+        
+        
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
     buffer = malloc(sizeof(char) * (letters));
     if (!buffer)
 		return (0);
-	ssize_t bytes_read, bytes_written;
-
-	if (!filename)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+	
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -36,5 +38,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	close(fd);
+	free(buffer);
 	return (bytes_written);
 }
