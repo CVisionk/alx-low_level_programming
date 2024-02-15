1ff3a2350a6b38008366b85fd32aac164512bb96
Collen <damd819@gmail.com>
2024-02-15 18:19:24 +0200

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,33 @@
  *
  */
 
+ */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t bytes_written;
-    int nletters;
 	int fd;
+	int nletters;
+	int rwr;
+
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
-	{
 		return (-1);
-	}
-    if (!text_content)
+
+	if (!text_content)
 		text_content = "";
-    
-    for (nletters = 0; text_content[nletters]; nletters++);
-	bytes_written = write(fd, text_content, nletters);
-	if (bytes_written == -1)
-	{
-		close(fd);
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
-	}
+
 	close(fd);
+
 	return (1);
 }
