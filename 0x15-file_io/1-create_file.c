d258d1840926a6b9150dbcac7ffbe98742843acb
Collen <damd819@gmail.com>
2024-02-16 01:37:17 +0200

    readfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 /**
  * struct int create_file -  creates a file.
  * @filename: name of file to create
@@ -34,4 +34,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,15 +10,20 @@
 
 int append_text_to_file(const char *filename, char *text_content){
     int fd;
-	int bytes_read, bytes_written;
-	char *buffer;
 
 	if (!filename)
-		return (0);
-
-	fd = open(filename, O_RDWR);
+		return (-1);
+	if(!text_content)
+		return (1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
-    bytes_written = write(filename, buffer, bytes_read);
+	if (write(fd, text_content, strlen(text_content)) == -1){
+		close(fd);
+		return(-1);
+	}
+
+	close(fd);
+    return (1);
 }
