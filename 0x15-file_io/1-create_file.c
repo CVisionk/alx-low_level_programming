3d52c9cf71ef9a38453f4a70b0e5c4c5721fa818
Collen <damd819@gmail.com>
2024-02-15 18:18:18 +0200

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t bytes_written;
+    int nletters;
 	int fd;
 	if (!filename)
 		return (-1);
@@ -22,8 +23,10 @@ int create_file(const char *filename, char *text_content)
 	}
     if (!text_content)
 		text_content = "";
-	bytes_written = write(fd, text_content, strlen(text_content));
-	if (bytes_written <= 0)
+    
+    for (nletters = 0; text_content[nletters]; nletters++);
+	bytes_written = write(fd, text_content, nletters);
+	if (bytes_written == -1)
 	{
 		close(fd);
 		return (-1);
