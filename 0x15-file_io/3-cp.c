b65e8414ad453ec5822c1b7eb1e58a41463a182b
Collen <damd819@gmail.com>
2024-02-16 14:26:11 +0200

    fixed

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+
+void print_error(const char *message, const char *filename, int status) {
+    dprintf(STDERR_FILENO, message, filename);
+    exit(status);
+}
+
+void close_file(int fd, int status) {
+    if (close(fd) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+        exit(status);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int fd_from, fd_to;
+    ssize_t n_read, n_written;
+    char buf[BUFFER_SIZE];
+
+    if (argc != 3) {
+        print_error("Usage: cp file_from file_to\n", NULL, 97);
+    }
+
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1) {
+        print_error("Error: Can't read from file %s\n", argv[1], 98);
+    }
+
+    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+    if (fd_to == -1) {
+        close_file(fd_from, 100);
+        print_error("Error: Can't write to %s\n", argv[2], 99);
+    }
+
+    while ((n_read = read(fd_from, buf, BUFFER_SIZE)) > 0) {
+        n_written = write(fd_to, buf, n_read);
+        if (n_written == -1 || n_written != n_read) {
+            close_file(fd_from, 100);
+            close_file(fd_to, 100);
+            print_error("Error: Can't write to %s\n", argv[2], 99);
+        }
+    }
+
+    if (n_read == -1) {
+        close_file(fd_from, 100);
+        close_file(fd_to, 100);
+        print_error("Error: Can't read from file %s\n", argv[1], 98);
+    }
+
+    close_file(fd_from, 100);
+    close_file(fd_to, 100);
+
+    return 0;
+}
