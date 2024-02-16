d5a10e914d67352c64539e1d1ed6dea4fe943580
Collen <damd819@gmail.com>
2024-02-16 23:25:25 +0200

    fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -37,7 +37,7 @@ void print_elf_header(const char *filename) {
         printf("%02x ", elf_header.e_ident[i]);
     }
     printf("\nClass: %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-    printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+   printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "little endian" : "big endian");
     printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
     printf("OS/ABI: %s\n", elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Unknown");
     printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
