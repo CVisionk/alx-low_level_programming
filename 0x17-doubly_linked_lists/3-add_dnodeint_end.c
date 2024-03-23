3083e757309f6e2fede55b405ed54f0229b4d467
Collen <damd819@gmail.com>
2024-03-23 17:10:48 +0200

    fixBetty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			current = current->next;
 		newNode->prev = current;
 		current->next = newNode;
-	}else
+	} else
 		*head = newNode;
 
 	return (newNode);
