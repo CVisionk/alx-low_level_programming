db0a86df8897ee6facf31246a43f524fe18e20f3
Collen <damd819@gmail.com>
2024-03-25 16:15:21 +0200

    created 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P ..
-
-LD_PRELOAD=/$PWD/lib_injectcode.so
+wget -P .. https://raw.githubusercontent.com/CVisionk/alx-low_level_programming/main/0x18-dynamic_libraries/lib_injectcode.so
+export LD_PRELOAD="$PWD/../lib_injectcode.so"
