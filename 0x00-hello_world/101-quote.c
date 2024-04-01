cc3dabaab8c2200f11c07f76a85f2d6ff1c73e1f
Collen <damd819@gmail.com>
2024-04-01 14:32:58 +0200

    bettyFix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - prints quote and a new line.
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-        write(2,
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
