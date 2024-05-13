56460c0747e88645330b05db95a56b686d6bdb2e
Collen <damd819@gmail.com>
2024-05-13 15:23:05 +0200

    fix syntax

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,6 +6,7 @@
  * main - Entry point, prints only last digit of num stored in n
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n, last_digit;
@@ -18,14 +19,11 @@ int main(void)
 	printf("Last digit of %d is %d and is ", n, last_digit);
 
 	if (last_digit > 5)
-	{
 		printf("greater than 5\n");
-	}
 	else if (last_digit == 0)
-	{
 		printf("0\n");
-	}
 	else
 		printf("less than 6 and not 0\n");
+
 	return (0);
 }
