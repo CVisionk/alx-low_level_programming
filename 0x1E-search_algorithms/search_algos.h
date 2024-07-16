73ea84d32186c13ca4ac36185db4742f26b6a3fb
Collen <damd819@gmail.com>
2024-07-16 14:07:11 +0200

    Created 0-linear

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,13 @@
+#ifndef SEARCH_ALGO_H
+#define SEARCH_ALGO_H
 
-
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * File: search_algos.h
+ * Auth: Collen Khoza
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x1E. C - Search Algorithms.
+ */
 
 int linear_search(int *array, size_t size, int value);
 
-
-#endif /* MAIN_H */
+#endif /* SEARCH_ALGO_H */
