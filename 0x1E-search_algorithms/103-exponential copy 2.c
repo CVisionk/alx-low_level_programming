cd138e173d153c3f06dde0ab5502446d96400fa6
Collen <damd819@gmail.com>
2024-07-20 09:58:08 +0200

    Modified 103-exponential.c

--- 0x1E-search_algorithms/103-exponential copy 2.c
+++ 0x1E-search_algorithms/103-exponential copy 2.c
@@ -4,18 +4,17 @@
 #include "search_algos.h"
 
 /**
- * binary_search - function that searches for a value in a sorted
+ * binary_search_e - function that searches for a value in a sorted
  *				 array of integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
+ * @low: range to search for value
+ * @high: range to search for value
  * @value: value to search for
  * Return: return the index where value is located, 0 otherwise
  */
 
-int binary_search(int *array, size_t size, int value)
+int binary_search_e(int *array, int value, size_t low, size_t high)
 {
-	size_t low = 0;
-	size_t high = size - 1;
 	size_t i;
 
 	if (array == NULL)
@@ -55,7 +54,6 @@ int binary_search(int *array, size_t size, int value)
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t pos = 1;
-	int temp_b = -1;
 
 	if (array == NULL || size == 0)
 		return (-1);
@@ -71,14 +69,12 @@ int exponential_search(int *array, size_t size, int value)
 		else
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", pos / 2, pos);
-			temp_b = binary_search(&array[pos / 2], (pos / 2), value);
-			return (temp_b == -1 ? -1 : (int)(temp_b + pos / 2));
+			return (binary_search_e(array, value, (pos / 2), pos));
 		}
 		if (pos >= size)
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", pos / 2, size - 1);
-			temp_b = binary_search(&array[pos / 2], (pos / 2), value);
-			return (temp_b == -1 ? -1 : (int)(temp_b + pos / 2));
+			return (binary_search_e(array, value, (pos / 2), size - 1));
 		}
 	}
 	return (-1);
