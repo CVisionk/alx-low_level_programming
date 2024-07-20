299a8a41fe6aeba0d800d4acdab11e602548cb7e
Collen <damd819@gmail.com>
2024-07-20 13:01:31 +0200

    Modified 104-advanced_binary

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,10 +5,10 @@
 /**
  * linear_search - function that searches for a value in an array of
  *                 integers using the Linear search algorithm
- * @array: pointer to the first element of the array to search in.
- * @size:  is the number of elements in array.
- * @value:  is the value to search for.
- * Return: first index where the value is located, -1 otherwise.
+ * @array: pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value:  is the value to search for
+ * Return: first index where the value is located, -1 otherwise
  */
 
 int linear_search(int *array, size_t size, int value)
