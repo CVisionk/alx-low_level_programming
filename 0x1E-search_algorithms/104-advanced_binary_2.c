a27585848bd24f1c78d27e7a510247a9bc031774
Collen <damd819@gmail.com>
2024-07-20 12:51:06 +0200

    Modified 104-advanced_binary

--- 0x1E-search_algorithms/104-advanced_binary_2.c
+++ 0x1E-search_algorithms/104-advanced_binary_2.c
@@ -4,7 +4,7 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - function that searches for a value in a sorted
+ * advanced_binary_helper - function that searches for a value in a sorted
  *				 array of integers using the recursive Binary search
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
@@ -12,7 +12,7 @@
  * Return: return the index where value is located, 0 otherwise
  */
 
-int advanced_binary_helper(int *array, size_t size, int value)
+int advanced_binary(int *array, size_t size, int value)
 {
 	size_t low = 0;
 	size_t high = size - 1;
@@ -37,33 +37,19 @@ int advanced_binary_helper(int *array, size_t size, int value)
 		temp = mid;
 		if (array[mid] == array[mid - 1])
 		{
-			temp = advanced_binary_helper(array, mid + 1, value);
+			temp = advanced_binary(array, mid + 1, value);
 		}
 		return (temp);
 	}
 	else if (array[mid] > value)
 	{
-		temp = advanced_binary_helper(array, mid - 1, value);
+		temp = advanced_binary(array, mid - 1, value);
 		return (temp == -1 ? temp : temp + ((int)mid - 1));
 	}
 	else
 	{
-		temp = advanced_binary_helper(&array[mid + 1], size - (mid + 1), value);
+		temp = advanced_binary(&array[mid + 1], size - (mid + 1), value);
 		return (temp == -1 ? temp : temp + ((int)mid + 1));
 	}
 	return (-1);
-}
-
-/**
- * advanced_binary - function that searches for a value in a sorted
- *				 array of integers using the recursive Binary search
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * Return: return the index where value is located, 0 otherwise
- */
-
-int advanced_binary(int *array, size_t size, int value)
-{
-	return (advanced_binary_helper(array, size, value));
 }
